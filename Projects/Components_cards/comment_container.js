621920b463056d6502f63e97e802d4931baae9b0
p2arthur <84791111+p2arthur@users.noreply.github.com>
2021-07-13 15:48:18 -0300

    Add files via upload

---
+++ Projects/Components_cards/comment_container.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react";
+
+const CommentContainer = () => {
+  return <div className="ui container comments"></div>;
+};
+
+export default CommentContainer;
